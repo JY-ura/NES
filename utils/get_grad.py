7449ad311b327c861a317c01598da0c6ffbf9e76
Ritian <ritianzhaoxx@gmail.com>
2023-11-15 20:36:30 +0800

    🐞 fix(get_grad.py): zoestimator
    zoestimator

--- utils/get_grad.py
+++ utils/get_grad.py
@@ -3,58 +3,83 @@ from typing import Tuple
 import torch.nn as nn
 from optimizer import margin_loss
 
-class GradEstimation:
-    def __init__(self, model, sample_per_draw, batch_size, sigma, targeted, norm_threshold, alpha, pre_k_grad, host) -> None:
+
+class ZOEstimator(nn.Module):
+    def __init__(
+        self,
+        model: nn.Module,
+        sample_num: int,
+        max_sample_num_per_forward: int,
+        sigma: float,
+        targeted: str,
+        grad_clip_threshold: float,
+        alpha: float,
+        subspace_dim: int,
+        device: torch.device
+    ) -> None:
+        assert targeted in ['targeted', 'untargeted']
+
         self.model = model
-        self.sample_per_draw = sample_per_draw
-        self.batch_size = batch_size
+        self.sample_num = sample_num
+        self.max_sample_num_per_forward = max_sample_num_per_forward
         self.sigma = sigma
         self.targeted = targeted
-        self.norm_threshold = norm_threshold
+        self.grad_clip_threshold = grad_clip_threshold
         self.alpha = alpha
-        self.pre_k_grad = pre_k_grad
-        self.host = host
-        self.sigma_sqrt = 0.1
-        self.grads = None
-        self.pre_grad_list = []
+        self.subspace_dim = subspace_dim
+        self.device = device
+        self.previous_grads = None
+        self.previous_grad_queue = []
         self.loss_func = margin_loss
 
-    def get_grad_estimation(
+    def forward(self, evaluate_img, target_labels):
+        return self(evaluate_img, target_labels)
+
+    def zo_estimation(
         self,
         evaluate_img: torch.Tensor,
         target_labels: torch.Tensor,
+        subspace_estimation: bool = False
     ) -> Tuple[torch.Tensor, torch.Tensor]:
 
-        total_batch_num = self.sample_per_draw // self.batch_size
+        num_forwards = self.sample_num // self.max_sample_num_per_forward
         total_grads = []
         total_loss = []
-        for _ in range(total_batch_num):
-            noise = torch.normal(mean=0.0, std=1.0, size=(
-                self.batch_size // 2,) + evaluate_img.shape[1:], device=self.host)
 
-            if self.grads is not None:
-                self.pre_grad_list.append(self.grads.flatten())
-                if len(self.pre_grad_list) == self.pre_k_grad:
-                    pre_grad = torch.stack(
-                        self.pre_grad_list, dim=0).to(self.host)
-                    noise = self.sample(
-                        pre_grad.transpose(0, 1),
+        for _ in range(num_forwards):
+            # determine wether to use subspace to sample perturbation
+            if not subspace_estimation or self.previous_grads is None:
+                perturbation = torch.normal(
+                    mean=0.0,
+                    std=1.0,
+                    size=(self.max_sample_num_per_forward // 2,) +
+                    evaluate_img.shape[1:],
+                    device=self.device
+                )
+
+            else:
+                self.previous_grad_queue.append(self.previous_grads.flatten())
+                if len(self.previous_grad_queue) == self.subspace_dim:
+                    previous_grads = torch.stack(
+                        self.previous_grad_queue, dim=0).to(self.device)
+                    perturbation = self.subspace_sample(
+                        previous_grads.transpose(0, 1),
                         evaluate_img.flatten().shape[0]
                     )
-                    noise = noise.view((noise.size(0),) +
-                                       evaluate_img[0].size())
-                    self.pre_grad_list.pop(0)
+                    perturbation = perturbation.view(
+                        (perturbation.size(0),) + evaluate_img[0].size())
+                    self.previous_grad_queue.pop(0)
 
             # generate + delta and - delta for evaluation
-            noise = torch.concat([noise, -noise], dim=0)
+            perturbation = torch.concat([perturbation, -perturbation], dim=0)
 
-            evaluate_imgs = evaluate_img + noise * self.sigma
+            evaluate_imgs = evaluate_img + perturbation * self.sigma
 
-            score = self.model(evaluate_imgs)
-            loss = self.loss_fn(score, target_labels, self.targeted)
+            prediction = self.model(evaluate_imgs)
+            loss = self.loss_fn(prediction, target_labels, self.targeted)
 
             loss = loss.reshape(-1, 1, 1, 1).repeat(evaluate_img.shape)
-            grad = loss * noise / self.sigma / 2
+            grad = loss * perturbation / self.sigma / 2
 
             total_grads.append(torch.mean(grad, dim=0, keepdim=True))
             total_loss.append(torch.mean(loss))
@@ -66,22 +91,36 @@ class GradEstimation:
             total_grads = total_grads / \
                 torch.norm(total_grads, p=2)*self.norm_theshold
         total_loss = torch.mean(torch.tensor(
-            total_loss, device=self.host), dim=0)
+            total_loss, device=self.device), dim=0)
+
+        self.previous_grads = total_grads.detach().clone()
         return total_grads, total_loss
 
-    def sample(self, grads, d):
-        noise_full = torch.rand(self.batch_size//2, d, 1).to(self.host)
-        nosie_subspace = torch.rand(
-            self.batch_size//2, self.pre_k_grad, 1).to(self.host)
+    def subspace_sample(self, grads: torch.Tensor, d: int) -> torch.Tensor:
+        """sample perturbation from subspace
 
+        Args:
+            grads (Tensor): previous gradients, used to formulate subspace
+            d (int): dimension of the optimization space
+
+        Returns:
+            torch.Tensor: perturbation sampled from subspace
+        """
+        perturbation_full_space = torch.randn(
+            self.max_sample_num_per_forward//2, d, 1, device=self.device)
+        perturbation_subspace = torch.randn(
+            self.max_sample_num_per_forward//2, self.subspace_dim, 1, device=self.device)
+
+        # use qr decomposition to get orthonormal basis
         q, _ = torch.qr(grads)
-        noise = self.sigma_sqrt * torch.sqrt(torch.tensor(1-self.alpha / d * 0.1)) * noise_full \
-            + self.sigma_sqrt * torch.sqrt(torch.tensor((self.alpha) /
-                                                        self.pre_k_grad * 1.0)) * torch.matmul(q, nosie_subspace)
-        return noise.squeeze(-1)
-    
+        perturbation = self.sigma * torch.sqrt(torch.tensor((1-self.alpha) / d * 1.0)) * perturbation_full_space \
+            + self.sigma * torch.sqrt(torch.tensor(self.alpha /
+                                                        self.subspace_dim * 1.0)) * torch.matmul(q, perturbation_subspace)
+        # q: [n, subspace_dim]
+        # perturbation_subspace: [max_sample_num, subspace_dim, 1]
+        return perturbation.squeeze(-1)
 
-    def compute_true_loss(self, evaluate_img: torch.Tensor, target_labels: torch.Tensor)->torch.Tensor:
+    def compute_true_loss(self, evaluate_img: torch.Tensor, target_labels: torch.Tensor) -> torch.Tensor:
         score = self.model(evaluate_img)
         loss = self.loss_func(score, target_labels, self.targeted)
         return torch.squeeze(loss, 0)
