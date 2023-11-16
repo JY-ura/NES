b70a07bb0935ed6afee00db8bbce9e3417d9aa59
yura <yura1213@163.com>
2023-11-16 15:46:41 +0800

    🦄 refactor(attack.py get_grad.py ): refeactor
    refeactor

--- utils/get_grad.py
+++ utils/get_grad.py
@@ -2,7 +2,7 @@ import torch
 from typing import Tuple
 import torch.nn as nn
 from optimizer import margin_loss
-
+import math
 
 class ZOEstimator(nn.Module):
     def __init__(
@@ -57,26 +57,27 @@ class ZOEstimator(nn.Module):
                     size=(self.max_sample_num_per_forward // 2,) +
                     evaluate_img.shape[1:],
                     device=self.device
-                )
+                ) * self.sigma
 
             else:
-                print("subspace estimation")
-                self.previous_grad_queue.append(self.previous_grads.flatten())
-                if len(self.previous_grad_queue) == self.subspace_dim:
-                    previous_grads = torch.stack(
-                        self.previous_grad_queue, dim=0).to(self.device)
-                    perturbation = self.subspace_sample(
-                        previous_grads.transpose(0, 1),
-                        evaluate_img.flatten().shape[0]
-                    )
-                    perturbation = perturbation.view(
-                        (perturbation.size(0),) + evaluate_img[0].size())
-                    self.previous_grad_queue.pop(0)
+                # print("subspace estimation")
+                # self.previous_grad_queue.append(self.previous_grads.flatten())
+                # if len(self.previous_grad_queue) == self.subspace_dim:
+
+                previous_grads = torch.stack(
+                    self.previous_grad_queue, dim=0).to(self.device)
+                perturbation = self.subspace_sample(
+                    previous_grads.transpose(0, 1),
+                    evaluate_img.flatten().shape[0],
+                    self.sigma
+                )
+                perturbation = perturbation.view(
+                    (perturbation.size(0),) + evaluate_img[0].size())
 
             # generate + delta and - delta for evaluation
             perturbation = torch.concat([perturbation, -perturbation], dim=0)
 
-            evaluate_imgs = evaluate_img + perturbation * self.sigma
+            evaluate_imgs = evaluate_img + perturbation
 
             prediction = self.model(evaluate_imgs)
             loss = self.loss_func(prediction, target_labels, self.targeted)
@@ -88,6 +89,9 @@ class ZOEstimator(nn.Module):
             total_loss.append(torch.mean(loss))
             # torch.cuda.empty_cache()
 
+        if len(self.previous_grad_queue) == self.subspace_dim:
+            self.previous_grad_queue.pop(0)
+
         total_grads = torch.mean(torch.concat(
             total_grads), dim=0, keepdim=True)
         if torch.norm(total_grads, p=2) > self.grad_clip_threshold:
@@ -97,14 +101,15 @@ class ZOEstimator(nn.Module):
             total_loss, device=self.device), dim=0)
 
         # self.previous_grads = total_grads.detach().clone()
-        return total_grads, total_loss
+        return total_grads, total_loss.detach()
 
-    def subspace_sample(self, grads: torch.Tensor, d: int) -> torch.Tensor:
+    def subspace_sample(self, grads: torch.Tensor, d: int, sigma: float) -> torch.Tensor:
         """sample perturbation from subspace
 
         Args:
             grads (Tensor): previous gradients, used to formulate subspace
             d (int): dimension of the optimization space
+            sigma (float): standard deviation of the gaussian noise
 
         Returns:
             torch.Tensor: perturbation sampled from subspace
@@ -116,9 +121,8 @@ class ZOEstimator(nn.Module):
 
         # use qr decomposition to get orthonormal basis
         q, _ = torch.qr(grads)
-        perturbation = self.sigma * torch.sqrt(torch.tensor((1-self.alpha) / d * 1.0)) * perturbation_full_space \
-            + self.sigma * torch.sqrt(torch.tensor(self.alpha /
-                                                        self.subspace_dim * 1.0)) * torch.matmul(q, perturbation_subspace)
+        perturbation = math.sqrt(sigma) * torch.sqrt(torch.tensor((1-self.alpha) / d)) * perturbation_full_space \
+            + math.sqrt(sigma) * torch.sqrt(torch.tensor(self.alpha/self.subspace_dim)) * torch.matmul(q, perturbation_subspace)
         # q: [n, subspace_dim]
         # perturbation_subspace: [max_sample_num, subspace_dim, 1]
         return perturbation.squeeze(-1)
